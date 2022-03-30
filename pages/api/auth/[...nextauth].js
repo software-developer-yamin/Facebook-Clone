e7143acece31f2de38d32df4fdd54902594f29f5
DeveloperYamin <developer.yamin@gmail.com>
2022-03-31 00:37:59 +0600

    remove facebook login

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -5,10 +5,6 @@ import GoogleProvider from "next-auth/providers/google";
 export default NextAuth({
      // Configure one or more authentication providers
      providers: [
-          FacebookProvider({
-               clientId: process.env.FACEBOOK_CLIENT_ID,
-               clientSecret: process.env.FACEBOOK_CLIENT_SECRET
-          }),
           GoogleProvider({
                clientId: process.env.GOOGLE_CLIENT_ID,
                clientSecret: process.env.GOOGLE_CLIENT_SECRET
